0a01f67130182a0e3ba179a68d074b11c641ff7b
ww1659 <ww1659@my.bristol.ac.uk>
2024-05-14 20:21:56 +0100

    pages layout

--- app/sales/page.tsx
+++ app/sales/page.tsx
@@ -1,9 +1,9 @@
 import Link from "next/link";
 
-export default function HomePage() {
+export default function SalesPage() {
   return (
     <main className="flex min-h-screen flex-col items-start justify-start px-12 py-5">
-      Home Page
+      Sales Page
     </main>
   );
 }
---
+++ components/Navbar.tsx
@@ -0,0 +1,62 @@
+"use client";
+
+import Link from "next/link";
+import { cn } from "@/lib/utils";
+import { usePathname } from "next/navigation";
+
+export default function Navbar({
+  className,
+  ...props
+}: React.HTMLAttributes<HTMLElement>) {
+  const pathname = usePathname();
+
+  return (
+    <div className="sticky top-0 bg-background w-full flex flex-row items-end justify-start border py-4 px-12">
+      <h4> Black Swan Linen</h4>
+      <nav
+        className={cn(
+          "flex items-center gap-4 text-sm lg:gap-6 ml-10",
+          className
+        )}
+        {...props}
+      >
+        <Link
+          href="/"
+          className={cn(
+            "transition-colors hover:text-foreground/80",
+            pathname === "/" ? "text-foreground" : "text-foreground/60"
+          )}
+        >
+          Home
+        </Link>
+        <Link
+          href="/sales"
+          className={cn(
+            "transition-colors hover:text-foreground/80",
+            pathname === "/sales" ? "text-foreground" : "text-foreground/60"
+          )}
+        >
+          Sales
+        </Link>
+        <Link
+          href="/customers"
+          className={cn(
+            "transition-colors hover:text-foreground/80",
+            pathname === "/customers" ? "text-foreground" : "text-foreground/60"
+          )}
+        >
+          Customers
+        </Link>
+        <Link
+          href="/orders"
+          className={cn(
+            "transition-colors hover:text-foreground/80",
+            pathname === "/orders" ? "text-foreground" : "text-foreground/60"
+          )}
+        >
+          Orders
+        </Link>
+      </nav>
+    </div>
+  );
+}
